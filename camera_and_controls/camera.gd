79fd1d4b1f0d802094583ce1ecaae8890b73df3d
cole-c <cole.e.cummings@gmail.com>
2025-08-01 19:22:28 -0500

    moved some logic from camera to game_controller

--- camera_and_controls/camera.gd
+++ camera_and_controls/camera.gd
@@ -1,6 +1,6 @@
 extends Camera3D
 
-@export var character: Character
+@export var game: GameController 
 
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
 
@@ -12,36 +12,19 @@ var selected_character: Character
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#TODO move most of this logic to game_controller and just update collider object here
 func _physics_process(delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100.0
 	ray_cast_3d.force_raycast_update()
-	#TODO this is a work around until game_controller can assign selected character
 	if(ray_cast_3d.is_colliding()):
 		var collider : RigidBody3D = ray_cast_3d.get_collider()
-		if(collider.is_in_group("Character") && collider.get_parent() != selected_character):
-			if(Input.is_action_just_pressed("click_L")):
-				if(selected_character):
-					selected_character.clear_movable_tiles()
-				selected_character = collider.get_parent()
-				selected_character.highlight_movable_tiles()
-	if(ray_cast_3d.is_colliding() && selected_character):
-		var collider : RigidBody3D = ray_cast_3d.get_collider()
+		if(collider.is_in_group("Character") && Input.is_action_just_pressed("click_L")):
+			game.click_character(collider.get_parent())
 		if (collider.is_in_group("Tile")):
 			var new_hovered_tile = collider.get_parent()
-			if(!hovered_tile):
-				hovered_tile = new_hovered_tile
-			if(new_hovered_tile != hovered_tile):
-				hovered_tile.is_hovered(false)
-				hovered_tile = new_hovered_tile
-			if(hovered_tile.get_movable()):
-				hovered_tile.is_hovered(true)
-			if(Input.is_action_just_pressed("click_L") && (selected_character.currentTile == null || hovered_tile.get_movable())):
-				selected_character.assignTile(hovered_tile)
+			game.hover_tile(new_hovered_tile)
+			if(Input.is_action_just_pressed("click_L")):
+				game.click_tile(new_hovered_tile)
 	else: 
-		if(hovered_tile):
-			hovered_tile.is_hovered(false)
-		hovered_tile = null
+		game.hover_default()
--- camera_and_controls/game_controller.gd
+++ camera_and_controls/game_controller.gd
@@ -5,6 +5,8 @@ class_name GameController
 @export var save_handler: SaveAndLoad
 
 var tiles = {}
+var selected_character: Character
+var hovered_tile: Tile
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
@@ -24,3 +26,30 @@ func map_tiles() -> void:
 	
 func get_key_from_tile(tile: Tile) -> String:
 	return str(tile.global_position.x) + "," + str(tile.global_position.z) + "," + str(tile.level)
+	
+func click_character(character: Character) -> void:
+	#TODO this is a work around until the game controller assigns a tile on character spawn, etc
+	if(selected_character && selected_character != character):
+		selected_character.clear_movable_tiles()
+	selected_character = character
+	selected_character.highlight_movable_tiles()
+	
+func hover_tile(tile: Tile) -> void:
+	var new_hovered_tile = tile
+	if(!hovered_tile):
+		hovered_tile = new_hovered_tile
+	if(new_hovered_tile != hovered_tile):
+		hovered_tile.is_hovered(false)
+		hovered_tile = new_hovered_tile
+	if(hovered_tile.get_movable()):
+		hovered_tile.is_hovered(true)
+	
+func click_tile(tile: Tile) -> void:
+	if(selected_character && (selected_character.currentTile == null || hovered_tile.get_movable())):
+		#TODO AssignTile currently can set or move between current and new tiles, eventually split these up and the game controller will set tiles on spawn for characters
+		selected_character.assignTile(hovered_tile)
+	
+func hover_default() -> void:
+	if(hovered_tile):
+		hovered_tile.is_hovered(false)
+	hovered_tile = null
